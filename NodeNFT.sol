621ee949bfbc5f500e04679e2a4483dcd42551cc
luxworldlabs <117621872+luxworldlabs@users.noreply.github.com>
2023-03-03 15:33:10 +0700

    feat: updated Node NFT

--- NodeNFT.sol
+++ NodeNFT.sol
@@ -1,24 +1,34 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NodeNFT is ERC721, ERC721Enumerable, Pausable, Ownable {
+contract NFTNode is ERC721Enumerable, Pausable, Ownable {
     using Counters for Counters.Counter;
-
+    using Strings for uint256;
+    string public baseExtension = ".json";
     Counters.Counter private _tokenIdCounter;
-
-    struct infoLuggage{
-        string qualityVa;
+    struct infoNode{
+        uint8 qualityVa;
     }
 
-    mapping(uint256 => infoLuggage) public infoLuggageList;
+    mapping(uint256 => infoNode) public infoNodeList;
 
-    constructor() ERC721("Node NFT", "Node") {}
+    constructor() ERC721("NFT Node", "Node") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://metadata.lux.world/node/";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        _requireMinted(tokenId);
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), baseExtension)) : "";
+    }
 
     function pause() public onlyOwner {
         _pause();
@@ -34,31 +44,26 @@ contract NodeNFT is ERC721, ERC721Enumerable, Pausable, Ownable {
         _safeMint(to, tokenId);
     }
 
-    function batchMint(address toAddress, uint number, string memory _quality) public onlyOwner {
+    function batchMint(address toAddress, uint number, uint8 _quality) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
-        
         for (uint i = 0; i < number; i++) {
             _tokenIdCounter.increment();
             tokenId = _tokenIdCounter.current();
-           _safeMint(toAddress, tokenId);
-           infoLuggageList[tokenId] = infoLuggage( _type,_quality);
+            _safeMint(toAddress, tokenId);
+            infoNodeList[tokenId] = infoNode(_quality);
         }
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        whenNotPaused
-        override(ERC721, ERC721Enumerable)
+    function transferWithAmountNft(address _receiverAdd, uint256[] memory _tokenID) public
     {
-        super._beforeTokenTransfer(from, to, tokenId);
+        for(uint i=0; i < _tokenID.length; i++)
+        {
+            _transfer(msg.sender, _receiverAdd, _tokenID[i]);
+        }
     }
-    
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
+
+    function viewInfoNode(uint256 _tokenId) external view returns(uint8)
     {
-        return super.supportsInterface(interfaceId);
+        return (infoNodeList[_tokenId].qualityVa);
     }
 }
